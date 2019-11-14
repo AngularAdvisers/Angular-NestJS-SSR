591739d4cb5f26a9158123c5bba2172d1d989f8a
Angular Advisors <AngularAdvisors@gmail.com>
2019-11-14 09:28:42 -0700

    Added firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyDVoFO7ZsVLq_cTJpDsXPCyfxcWjG2i_tQ',
+    authDomain: 'angular-ssr-nestjs.firebaseapp.com',
+    databaseURL: 'https://angular-ssr-nestjs.firebaseio.com',
+    projectId: 'angular-ssr-nestjs',
+    storageBucket: 'angular-ssr-nestjs.appspot.com',
+    messagingSenderId: '473099260685',
+    appId: '1:473099260685:web:b83a802b0eaa3586261644'
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: 'AIzaSyDVoFO7ZsVLq_cTJpDsXPCyfxcWjG2i_tQ',
+    authDomain: 'angular-ssr-nestjs.firebaseapp.com',
+    databaseURL: 'https://angular-ssr-nestjs.firebaseio.com',
+    projectId: 'angular-ssr-nestjs',
+    storageBucket: 'angular-ssr-nestjs.appspot.com',
+    messagingSenderId: '473099260685',
+    appId: '1:473099260685:web:b83a802b0eaa3586261644'
+  }  
 };
 
 /*
