e5e7e9fa6ae2c6762a366f79a456c5a8b32f7eb2
Angular Advisors <AngularAdvisors@gmail.com>
2019-11-14 10:34:21 -0700

    Added Cloud function settings

--- README.md
+++ README.md
@@ -178,6 +178,12 @@ firebase-debug.log
 ng add @nestjs/ng-universal
 ```
 
+## Add Firebase Polyfills to Express
+
+```
+xhr2 bufferutil utf-8-validate
+```
+
 ## Build
 
 Run `ng build` to build the project. The build artifacts will be stored in the `dist/` directory. Use the `--prod` flag for a production build.
--- server/main.ts
+++ server/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { ApplicationModule } from './app.module';
-
+import * as express from 'express';
+export const app = express();
 async function bootstrap() {
   const app = await NestFactory.create(ApplicationModule);
   app.setGlobalPrefix('api');
-  await app.listen(4200);
+  // Using Firebase Cloud Function Instead
+  // await app.listen(4200);
 }
 bootstrap();
